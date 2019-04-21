86c420c01fc8e40f31bde5aeb76cded53ea5cfea
salavatz <salavat240796@mail.ru>
2019-04-22 02:02:13 +0300

    fix 1 task

--- src/part01/lesson02/task01/Main.java
+++ src/part01/lesson02/task01/Main.java
@@ -3,6 +3,7 @@ package part01.lesson02.task01;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello, World!");
         //nullPointerExceptionModeling();
         //arrayIndexOutOfBoundsExceptionModeling();
         myExceptionModeling();
