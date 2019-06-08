e2fc69fc97c5f9a4f62007701852d767e31a5675
salavatz <salavat240796@mail.ru>
2019-06-09 01:13:30 +0300

    add 1 task for lesson 04 in part02

---
+++ src/part02/lesson04/Main.java
@@ -0,0 +1,8 @@
+package part02.lesson04;
+
+public class Main {
+    public static void main(String[] args) {
+        //Person person = new Person("Alex",22,"male");
+        //System.out.println(person.getName() + " " + person.getAge() + " " + person.getSex());
+    }
+}
